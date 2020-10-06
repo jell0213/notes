843d7c728762171440e2b1e8920f756cd5a4d468
jell0213 <jjason41004tw@gmail.com>
2020-10-06 14:40:49 +0800

    Update python指令.py

--- python指令.py
+++ python指令.py
@@ -1,3 +1,4 @@
+#######################################  excel 檔案
 wb = Workbook()
 ws = wb.active
 ws.append([folder_name,"mod="+str(num_mod)])
@@ -17,3 +18,22 @@ ws.append([file_name,
                str(round(compress_image_lc,3))+' %',
                str(round(compress_code_image_lc,3))+' %'])            
 wb.save(dir_out)#寫檔後存檔
+
+
+#######################################計時器
+# -*- coding: utf-8 -*-
+import time
+tStart = time.time()#計時開始
+ 
+#模擬要測量的function
+time.sleep(2)
+print "abc"
+for x in range(1000):
+    x += 1
+    print x
+#end of 模擬要測量的function
+ 
+tEnd = time.time()#計時結束
+#列印結果
+print "It cost %f sec" % (tEnd - tStart)#會自動做近位
+print tEnd - tStart#原型長這樣
